aef736a063c82502ef5054e691d52ba95467c11f
Ppamo <pablo@ppamo.cl>
2017-12-30 18:48:05 -0300

    Add server class

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Ppamo/go.sidecar.ambassador/config"
+	"github.com/Ppamo/go.sidecar.ambassador/server"
 )
 
 func main() {
@@ -10,5 +11,6 @@ func main() {
 	if e != nil {
 		fmt.Printf("Error generating conf\n%v\n", e)
 	}
+	server.StartServer(config)
 	fmt.Printf("server port: %v\n", config.Server.Port)
 }
