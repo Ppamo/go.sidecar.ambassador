3eb4a32d3244161cb6df429a724ebedcb9372c3b
Pablo Mansilla Ojeda <pablo@ppamo.cl>
2017-12-30 01:39:36 -0300

    Update GOPATH if the script is running on cygwin

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@ DOCKERIMAGE="${DOCKERIMAGE:-docker.io/golang:1.9.2-alpine}"
 PROJECT=$1
 BINARYFILE=$2
 
+OS=$(uname -o)
+if [ "$OS" == "Cygwin" ]
+then
+	GOPATH=${GOPATH#/cygdrive}
+fi
+
 CMD="cd \"src/$PROJECT\" && \
-       go get && \
-       CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o \"$BINARYFILE\" ."
+	go get && \
+	CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o \"$BINARYFILE\" ."
 
 docker run --rm --privileged=true -i -v "$GOPATH:/go" "$DOCKERIMAGE" /bin/sh -c "$CMD"
