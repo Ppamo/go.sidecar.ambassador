6e0d1be9d1cb054de18f4c9bac99b4d0bdcb52c5
Pablo Mansilla Ojeda <pablo@ppamo.cl>
2018-02-16 15:24:55 -0300

    First version of parent

--- dummy/dummy.go
+++ dummy/dummy.go
@@ -1,45 +1,43 @@
 package main
 
-// curl -v http://localhost:8081/serviceInfo?item=validation
-
 import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
 )
 
 var server *http.Server
 
 func main() {
-	fmt.Printf("=> starting parent process\n")
-	server = &http.Server{Addr: "localhost:8081"}
+	fmt.Printf("=> starting chad process at port 8081\n")
+	server = &http.Server{Addr: "0.0.0.0:8081"}
 	http.HandleFunc("/", requestHandler)
 	e := server.ListenAndServe()
 	if e != nil {
-		fmt.Printf("Error launching the server\n%v\n", e)
+		fmt.Printf("- Error launching the server\n%v\n", e)
 	}
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-	if strings.EqualFold(r.Method, "get") &&
+	if strings.EqualFold(r.Method, "get") && strings.EqualFold(r.RequestURI, "/"){
+		fmt.Fprintf(w, "{\"status\": \"ok\"}")
+	} else if strings.EqualFold(r.Method, "get") &&
 		strings.EqualFold(r.RequestURI, "/serviceInfo?item=validation") {
-		filepath := os.Args[1]
+		filepath := "validation.rules.json"
 		data, e := ioutil.ReadFile(filepath)
 		if e != nil {
 			fmt.Printf("error: loading file %s\n%v\n", filepath, e)
 			panic(e)
 		}
-		fmt.Printf("=> Returning data from file %s\n", filepath)
 		fmt.Fprintf(w, "%s", data)
 	} else if strings.EqualFold(r.Method, "get") &&
 		strings.EqualFold(r.RequestURI, "/serviceInfo?item=quit") {
-		fmt.Printf("=> quiting!")
+		fmt.Printf("+ quiting!")
 		server.Shutdown(nil)
 	} else {
-		fmt.Printf("=> call with path %s\n", r.RequestURI)
+		fmt.Printf("- call with path %s\n", r.RequestURI)
 		fmt.Fprintf(w, "{\"hello\": \"world\"}\n")
-		fmt.Printf("=> Returning default response\n")
+		fmt.Printf("- Returning default response\n")
 	}
 }
