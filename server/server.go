b6a14d95e686002d0e840e27154c0fbaa65beef3
Ppamo <pablo@ppamo.cl>
2018-03-25 03:36:44 -0300

    Update server

--- server/server.go
+++ server/server.go
@@ -3,8 +3,26 @@ package server
 import (
 	"fmt"
 	"github.com/Ppamo/go.sidecar.ambassador/config"
+	"log"
 	"net/http"
+	"os"
 )
+
+var server *http.Server
+var serverConfig config.Configuration
+
+func StartServer(config config.Configuration) error {
+	port, ok := os.LookupEnv("serverport")
+	if !ok {
+		port = fmt.Sprintf("%d", config.Server.Port)
+	}
+	address := fmt.Sprintf("0.0.0.0:%s", port)
+	log.Printf("* Starting server at %s\n", address)
+	server = &http.Server{Addr: address}
+	http.HandleFunc("/", requestHandler)
+	err := server.ListenAndServe()
+	return err
+}
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello my world!\n")
@@ -15,10 +33,4 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("url: %s\n", r.URL.Path)
 	fmt.Printf("query: %v\n", r.URL.Query())
 	fmt.Printf("postform: %s\n", r.PostForm)
-}
-
-func StartServer(config config.Configuration) error {
-	http.HandleFunc("/", requestHandler)
-	http.ListenAndServe(fmt.Sprintf("%s:%v", config.Server.Host, config.Server.Port), nil)
-	return nil
 }
