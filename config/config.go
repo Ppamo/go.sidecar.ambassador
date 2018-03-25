de45810ba5f8c55af037375df8e523e745ff8023
Ppamo <pablo@ppamo.cl>
2018-03-25 15:33:07 -0300

    Complete config lib, load values from config.json and set as environmental vars

--- config/config.go
+++ config/config.go
@@ -2,27 +2,71 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"reflect"
 )
 
 type Configuration struct {
 	Server ServerConfig `json:"server"`
-	Host   HostConfig   `json:"parent"`
+	Host   HostConfig   `json:"host"`
 }
 
 type ServerConfig struct {
-	Host string `json: "host"`
-	Port int    `json:"port"`
+	Host string `json:"host" env:"SERVERHOST"`
+	Port int    `json:"port" env:"SERVERPORT"`
 }
 
 type HostConfig struct {
-	Destination    string `json:"destination"`
-	UrlPrefix      string `json:"urlPrefix"`
-	ServiceInfoUrl string `json:"serviceInfoUrl"`
+	Destination    string `json:"destination" env:"DESTINATION"`
+	UrlPrefix      string `json:"urlPrefix" env:"URLPREFIX"`
+	ServiceInfoUrl string `json:"serviceInfoUrl" env:"SERVICEINFO"`
 }
 
-func LoadConfig(configPath string) (Configuration, error) {
+func setValues(item interface{}) {
+	var stringVal string
+	rt := reflect.TypeOf(item)
+	rv := reflect.ValueOf(item)
+
+	for i := 0; i < rt.NumField(); i++ {
+		key := rt.Field(i).Tag.Get("env")
+		value := rv.Field(i).Interface()
+		if rv.Field(i).Kind() == reflect.Struct {
+			setValues(value)
+			continue
+		}
+		if len(key) > 0 {
+			switch rv.Field(i).Kind() {
+			case reflect.String:
+				stringVal = fmt.Sprintf("%s", value.(string))
+			case reflect.Int32, reflect.Int, reflect.Int64:
+				stringVal = fmt.Sprintf("%d", value.(int))
+			case reflect.Float32:
+				stringVal = fmt.Sprintf("%f", value.(float32))
+			case reflect.Float64:
+				stringVal = fmt.Sprintf("%f", value.(float64))
+			case reflect.Bool:
+				stringVal = fmt.Sprintf("%b", value.(bool))
+			}
+			_, ok := os.LookupEnv(key)
+			if !ok {
+				log.Printf("+ Seting %s=%s\n", key, stringVal)
+				os.Setenv(key, value.(string))
+			} else {
+				log.Printf("+ Skipping %s=%s\n", key, stringVal)
+			}
+		}
+	}
+}
+
+func setConfigValues(config *Configuration) error {
+	setValues(config.Server)
+	setValues(config.Host)
+	return nil
+}
+
+func LoadConfig(configPath string) error {
 	var config Configuration
 	file, e := os.Open(configPath)
 	defer file.Close()
@@ -32,5 +76,10 @@ func LoadConfig(configPath string) (Configuration, error) {
 	}
 	parser := json.NewDecoder(file)
 	e = parser.Decode(&config)
-	return config, e
+	if e != nil {
+		log.Fatalf("- Error parsing config json\n%v\n", e)
+		panic(e)
+	}
+	e = setConfigValues(&config)
+	return e
 }
